3d1e5551111b628bf6547fe217ea7b4f76243124
Dimitrios Mavrommatis <d.mavrommatis@elsevier.com>
2020-09-15 21:20:28 +0200

    added mongodb container
    Signed-off-by: Dimitrios Mavrommatis <d.mavrommatis@elsevier.com>

--- apps/artemis-web/pages/api/auth.tsx
+++ apps/artemis-web/pages/api/auth.tsx
@@ -1,58 +1,65 @@
-const MongoClient = require("mongodb").MongoClient;
-const assert = require("assert");
-const bcrypt = require("bcrypt");
-const jwt = require("jsonwebtoken");
-const jwtSecret = "SUPERSECRETE20220";
+import assert from 'assert';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+
+const MongoClient = require('mongodb').MongoClient;
+const jwtSecret = 'SUPERSECRETE20220';
 
 const saltRounds = 10;
-const url = "mongodb://localhost:27017";
-const dbName = "artemis-web";
+const url = 'mongodb://admin:pass@localhost:27017';
+const dbName = 'artemis-web';
 
 const client = new MongoClient(url, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
 
-function findUser(db, email, callback) {
-  const collection = db.collection("user");
+function findUser(db: any, email: string, callback: any) {
+  const collection = db.collection('user');
   collection.findOne({ email }, callback);
 }
 
-function authUser(db, email, password, hash, callback) {
-  const collection = db.collection("user");
+function authUser(
+  db: any,
+  email: string,
+  password: string,
+  hash: string,
+  callback: any
+) {
+  const collection = db.collection('user');
   bcrypt.compare(password, hash, callback);
 }
 
-export default (req, res) => {
-  if (req.method === "POST") {
+export default (req: any, res: any) => {
+  if (req.method === 'POST') {
     console.log(req);
     //login
     try {
-      assert.notEqual(null, req.body.email, "Email required");
-      assert.notEqual(null, req.body.password, "Password required");
+      assert.notEqual(null, req.body.email, 'Email required');
+      assert.notEqual(null, req.body.password, 'Password required');
     } catch (bodyError) {
       res.status(403).send(bodyError.message);
     }
 
     client.connect(function (err) {
       assert.equal(null, err);
-      console.log("Connected to MongoDB server =>");
+      console.log('Connected to MongoDB server =>');
       const db = client.db(dbName);
       const email = req.body.email;
       const password = req.body.password;
 
       findUser(db, email, function (err, user) {
         if (err) {
-          res.status(500).json({ error: true, message: "Error finding User" });
+          res.status(500).json({ error: true, message: 'Error finding User' });
           return;
         }
         if (!user) {
-          res.status(404).json({ error: true, message: "User not found" });
+          res.status(404).json({ error: true, message: 'User not found' });
           return;
         } else {
           authUser(db, email, password, user.password, function (err, match) {
             if (err) {
-              res.status(500).json({ error: true, message: "Auth Failed" });
+              res.status(500).json({ error: true, message: 'Auth Failed' });
             }
             if (match) {
               const token = jwt.sign(
@@ -65,7 +72,7 @@ export default (req, res) => {
               res.status(200).json({ token });
               return;
             } else {
-              res.status(401).json({ error: true, message: "Auth Failed" });
+              res.status(401).json({ error: true, message: 'Auth Failed' });
               return;
             }
           });
---
+++ apps/artemis-web/pages/api/me.tsx
@@ -0,0 +1,27 @@
+import jwt from 'jsonwebtoken';
+const jwtSecret = 'SUPERSECRETE20220';
+
+export default (req: any, res: any) => {
+  if (req.method === 'GET') {
+    if (!('token' in req.cookies)) {
+      res.status(401).json({ message: 'Unable to auth' });
+      return;
+    }
+    let decoded: string | object;
+    const token = req.cookies.token;
+    if (token) {
+      try {
+        decoded = jwt.verify(token, jwtSecret);
+      } catch (e) {
+        console.error(e);
+      }
+    }
+
+    if (decoded) {
+      res.json(decoded);
+      return;
+    } else {
+      res.status(401).json({ message: 'Unable to auth' });
+    }
+  }
+};
