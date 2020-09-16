1e6f73ffb4222fb2d422e610e37f157d00ae309a
Dimitrios Mavrommatis <d.mavrommatis@elsevier.com>
2020-09-16 19:07:11 +0200

    format fix
    Signed-off-by: Dimitrios Mavrommatis <d.mavrommatis@elsevier.com>

--- apps/artemis-web/components/header/header.spec.tsx
+++ apps/artemis-web/components/header/header.spec.tsx
@@ -3,9 +3,9 @@ import { render } from '@testing-library/react';
 import { configure, shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import Header from './header';
-import Link from "next/link";
+import Link from 'next/link';
 
-configure({adapter: new Adapter()});
+configure({ adapter: new Adapter() });
 
 describe('Header', () => {
   it('should render successfully', () => {
@@ -14,16 +14,12 @@ describe('Header', () => {
   });
 
   it('should render the login, create account if logged in', () => {
-    let wrapper;
-
-    wrapper = shallow(<Header loggedIn={false} />);
+    const wrapper = shallow(<Header loggedIn={false} />);
     expect(wrapper.find(Link)).toHaveLength(2);
   });
 
   it('should render the overview, bgp updates, hijack pages, logout if logged in', () => {
-    let wrapper;
-
-    wrapper = shallow(<Header loggedIn={true} />);
+    const wrapper = shallow(<Header loggedIn={true} />);
     expect(wrapper.find(Link)).toHaveLength(3);
   });
 });
--- apps/artemis-web/pages/_app.tsx
+++ apps/artemis-web/pages/_app.tsx
@@ -1,4 +1,4 @@
-import "./styles.css";
-import App from "next/app";
+import './styles.css';
+import App from 'next/app';
 
 export default App;
