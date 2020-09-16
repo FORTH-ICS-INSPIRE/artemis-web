53173def7c619153cec7e5de7a8b47fbc016f0e5
kornilak <curiouz_k0d3r@protonmail.com>
2020-09-16 11:45:39 +0300

    simple header test

--- apps/artemis-web/components/header/header.spec.tsx
+++ apps/artemis-web/components/header/header.spec.tsx
@@ -1,11 +1,29 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-
+import { configure, shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 import Header from './header';
+import Link from "next/link";
+
+configure({adapter: new Adapter()});
 
 describe('Header', () => {
   it('should render successfully', () => {
     const { baseElement } = render(<Header />);
     expect(baseElement).toBeTruthy();
+  });
+
+  it('should render the login, create account if logged in', () => {
+    let wrapper;
+
+    wrapper = shallow(<Header loggedIn={false} />);
+    expect(wrapper.find(Link)).toHaveLength(2);
+  });
+
+  it('should render the overview, bgp updates, hijack pages, logout if logged in', () => {
+    let wrapper;
+
+    wrapper = shallow(<Header loggedIn={true} />);
+    expect(wrapper.find(Link)).toHaveLength(3);
   });
 });
