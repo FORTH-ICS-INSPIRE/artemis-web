f75d6280b01f4b0abcdbaa0f0b7bab40061b3735
kornilak <curiouz_k0d3r@protonmail.com>
2020-09-23 12:51:21 +0300

    next-auth db access

--- apps/artemis-web/pages/api/auth/[...nextauth].js
+++ apps/artemis-web/pages/api/auth/[...nextauth].js
@@ -1,5 +1,42 @@
 import NextAuth from 'next-auth'
 import Providers from 'next-auth/providers'
+const MongoClient = require('mongodb').MongoClient;
+const jwtSecret = 'SUPERSECRETE20220';
+import bcrypt from 'bcrypt';
+import useSWR from "swr";
+
+const saltRounds = 10;
+const url = 'mongodb://admin:pass@localhost:27017';
+const dbName = 'artemis-web';
+let db = null;
+const client = new MongoClient(url, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+client.connect(err => {
+  if (err) {
+    console.error('[mongo] client err', err);
+    return reject(err);
+  }
+
+  console.log('[mongo] connected');
+  db = client.db(dbName);
+});
+
+const findUser = (db, email, callback) => {
+  const collection = db.collection('user');
+  collection.findOne({ email }, callback);
+};
+
+const authUser = (
+  db,
+  email,
+  password,
+  hash,
+  callback
+) => {
+  bcrypt.compare(password, hash, callback);
+};
 
 const options = {
   // Configure one or more authentication providers
@@ -8,9 +45,70 @@ const options = {
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET
     }),
+    Providers.Credentials({
+      // The name to display on the sign in form (e.g. 'Sign in with...')
+      name: 'Credentials',
+      // The credentials is used to generate a suitable form on the sign in page.
+      // You can specify whatever fields you are expecting to be submitted.
+      // e.g. domain, username, password, 2FA token, etc.
+      credentials: {
+        username: { label: "email", type: "text" },
+        password: {  label: "password", type: "password" }
+      },
+      authorize: async (credentials) => {
+          const email = credentials.email;
+          const password = credentials.password;
+          let hash;
+          let isValidUser = false;
+          let myUser;
+          await db.collection('user').findOne({ email: email }).then((err, user) => {
+            hash = err.password;
+            myUser = err;
+            // console.log(myUser)
+          });
+          await bcrypt.compare(password, hash).then((match, err) => isValidUser = match);
+
+        // const muser = { image: 1, name: 'J Smith', email: 'aaaa@example.com' }
+        // return Promise.resolve(muser)
+        if (isValidUser) {
+          // Any object returned will be saved in `user` property of the JWT
+          return Promise.resolve(myUser)
+        } else {
+          console.log("errro")
+          // If you return null or false then the credentials will be rejected
+          return Promise.resolve(null)
+          // You can also Reject this callback with an Error or with a URL:
+          // return Promise.reject(new Error('error message')) // Redirect to error page
+          // return Promise.reject('/path/to/redirect')        // Redirect to a URL
+        }
+      },
+      callbacks: {
+        redirect: async (url, baseUrl) => {
+          return Promise.resolve(baseUrl)
+        },
+        session: async (session, user) => {
+          return Promise.resolve(session)
+        },
+        jwt: async (token, user, account, profile, isNewUser) => {
+          return Promise.resolve(token)
+        }
+    }
+    }),
     // ...add more providers here
   ],
-
+  pages: {
+    signIn: '/login'
+  },
+  events: {
+    session: async (message) => { console.log(message) },
+  },
+  session: {
+    jwt: true, 
+  },
+  jwt: {
+    // A secret to use for key generation (you should set this explicitly)
+    secret: 'INp8IvdIyeMcoGAgFGoA61DdBglwwSqnXJZkgz8PSnw', 
+  },
   // A database is optional, but required to persist accounts in a database
   database: process.env.DATABASE_URL,
 }
--- apps/artemis-web/pages/login.tsx
+++ apps/artemis-web/pages/login.tsx
@@ -5,28 +5,37 @@ import useSWR from "swr";
 import Head from "next/head";
 import { signIn, signOut, useSession } from 'next-auth/client';
 import cookie from 'js-cookie';
+import { csrfToken, getSession } from 'next-auth/client'
 
-const Login: React.FunctionComponent<{}> = () => {
+async function myFunction() {
+  const session = await getSession()
+  // console.log(session)
+  /* ... */
+}
+
+const Login = ({ csrfToken }) => {
+  
   const [ session, loading ] = useSession();
+  // myFunction();
 
-  const { data } = useSWR("/api/me", async function (args) {
-    const res = await fetch(args);
-    return res.json();
-  });
+  // const { data } = useSWR("/api/me", async function (args) {
+  //   const res = await fetch(args);
+  //   return res.json();
+  // });
 
-  if (!data || loading) return <h1>Loading...</h1>;
-  let loggedIn = false;
+  // if (!data || loading) return <h1>Loading...</h1>;
+  // let loggedIn = false;
 
-  if (data.email || session) {
-    loggedIn = true;
-    // Router.push("/overview");
-  }
-  const logout = () => {
-    cookie.remove('token');
-    //   revalidate();
-    Router.push('/');
-    // window.location.reload(false);
-  }
+  // if (data.email || session) {
+  //   loggedIn = true;
+  //   // Router.push("/overview");
+  // }
+  // const logout = () => {
+  //   cookie.remove('token');
+  //   //   revalidate();
+  //   Router.push('/');
+  //   // window.location.reload(false);
+  // }
 
   const Footer = dynamic(() => import("../components/footer/footer"));
   const SignIn2 = dynamic(() => import("../components/sign-in/sign-in"));
@@ -38,9 +47,9 @@ const Login: React.FunctionComponent<{}> = () => {
         <title>ARTEMIS - Login</title>
       </Head>
       <div id="login-container">
-        <Header loggedIn={loggedIn} call={session? signOut: logout} />
+        <Header loggedIn={false} call={signOut} />
         <div id="content-wrap" style={{ paddingBottom: "5rem" }}>
-          {!loggedIn && !session && <SignIn2 call={signIn('github')} />}
+        <SignIn2 csrf={csrfToken} />
         </div>
         <Footer />
       </div>
@@ -48,4 +57,9 @@ const Login: React.FunctionComponent<{}> = () => {
   );
 };
 
+Login.getInitialProps = async (context) => {
+  return {
+    csrfToken: await csrfToken(context)
+  }
+}
 export default Login;
