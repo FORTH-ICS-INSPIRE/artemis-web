d5efebef9d6a060626f8b7c770ff93f59221969f
Dimitrios Mavrommatis <jim.mavrommatis@gmail.com>
2020-09-24 16:52:49 +0200

    fix login

--- apps/artemis-web/pages/api/auth/[...nextauth].js
+++ apps/artemis-web/pages/api/auth/[...nextauth].js
@@ -10,8 +10,9 @@ const saltRounds = 10;
 const url = 'mongodb://admin:pass@localhost:27017';
 const dbName = 'artemis-web';
 
+let db;
+
 const initializeDB = (dbName) => {
-  let db;
   const client = new MongoClient(url, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
@@ -42,62 +43,62 @@ const options = {
       // The name to display on the sign in form (e.g. 'Sign in with...')
       name: 'Credentials',
       authorize: async (credentials) => {
-        const email = credentials.email;
-        const password = credentials.password;
-        const username = credentials.username;
-        let hash;
-        let isValidUser = false;
-        let myUser;
-        if (credentials.stype === 'signin') {
-          await db
-            .collection('user')
-            .findOne({ email: email })
-            .then((err, user) => {
-              hash = err.password;
-              myUser = err;
-              // console.log(myUser)
-            });
-          await bcrypt
-            .compare(password, hash)
-            .then((match, err) => (isValidUser = match));
-
-          if (isValidUser) {
+        try {
+          const email = credentials.email;
+          const password = credentials.password;
+          const username = credentials.username;
+          let isValidUser = false,
+            currUser;
+          if (credentials.stype === 'signin') {
             await db
               .collection('user')
-              .update({ email: email }, { $set: { lastLogin: new Date() } });
-            // Any object returned will be saved in `user` property of the JWT
-            return Promise.resolve(myUser);
+              .findOne({ email: email })
+              .then((user, err) => {
+                currUser = user;
+              });
+            if (!currUser) {
+              return Promise.resolve(null);
+            }
+            await bcrypt
+              .compare(password, currUser.password)
+              .then((match, err) => (isValidUser = match));
+
+            if (isValidUser) {
+              await db
+                .collection('user')
+                .update({ email: email }, { $set: { lastLogin: new Date() } });
+              // Any object returned will be saved in `user` property of the JWT
+              return Promise.resolve(currUser);
+            } else {
+              console.log('errro');
+              // If you return null or false then the credentials will be rejected
+              return Promise.resolve(null);
+            }
           } else {
-            console.log('errro');
-            // If you return null or false then the credentials will be rejected
-            return Promise.resolve(null);
-          }
-        } else {
-          console.log('signup');
-          let mHash;
-          await bcrypt
-            .hash(password, saltRounds)
-            .then((hash) => (mHash = hash));
-          // Store hash in your password DB.
-          await db
-            .collection('user')
-            .insertOne({
+            let mHash;
+            await bcrypt.hash(password, saltRounds).then((hash) => {
+              mHash = hash;
+            });
+            console.log(mHash);
+            // Store hash in your password DB.
+            let newUser = {
               userId: v4(),
-              username,
-              email,
-              password: hash,
+              username: username,
+              email: email,
+              password: mHash,
               role: 'user',
               lastLogin: new Date(),
-            })
-            .then((err) => err);
+            };
+            await db
+              .collection('user')
+              .insertOne(newUser)
+              .then((err) => err);
 
-          return Promise.resolve({
-            userId: v4(),
-            username,
-            email,
-            role: 'user',
-            lastLogin: new Date(),
-          });
+            return Promise.resolve(newUser);
+          }
+        } catch (err) {
+          console.error(err);
+          return Promise.resolve(null);
         }
       },
     }),
@@ -127,6 +128,9 @@ const options = {
   },
   events: {
     session: async (message) => {},
+    error: async (message) => {
+      console.error(message);
+    },
   },
   session: {
     jwt: true,
