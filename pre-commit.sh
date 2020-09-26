966bc3a1062d1fbc42a0bc329be8a153f1cb0cc4
kornilak <curiouz_k0d3r@protonmail.com>
2020-09-26 09:39:53 +0300

    pre-commit script

---
+++ pre-commit.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+yarn run format;
+
+branch="$(git rev-parse --abbrev-ref HEAD)"
+
+if [ "$branch" = "master" ]; then
+	  echo "You can't commit directly to master branch"
+	    exit 1
+fi
