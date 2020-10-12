864abff2089e977b3c9199de4d3c17744d1a3327
CuriouzK0d3r <curiouz_k0d3r@protonmail.com>
2020-10-12 18:12:20 +0300

    apollo-ws

--- apps/artemis-web/utils/constants.ts
+++ apps/artemis-web/utils/constants.ts
@@ -1,4 +1,5 @@
 const GRAPHQL_URI = 'http://localhost:9999/v1/graphql';
+const GRAPHQL_WEBSOCKETS_URI = 'ws://localhost:9999/v1/graphql';
 const HASURA_SECRET = '@rt3m1s.';
 
-export default { GRAPHQL_URI, HASURA_SECRET };
+export default { GRAPHQL_URI, HASURA_SECRET, GRAPHQL_WEBSOCKETS_URI };
--- apps/artemis-web/utils/graphql.ts
+++ apps/artemis-web/utils/graphql.ts
@@ -5,6 +5,11 @@ import {
   createHttpLink,
   gql,
 } from '@apollo/client';
+import { split } from 'apollo-link';
+import { HttpLink } from 'apollo-link-http';
+import { WebSocketLink } from 'apollo-link-ws';
+import { getMainDefinition } from 'apollo-utilities';
+
 import { useMemo } from 'react';
 import { setContext } from '@apollo/client/link/context';
 import constants from './constants';
@@ -24,6 +29,26 @@ const createApolloClient = () => {
       },
     };
   });
+
+  const wsLink = new WebSocketLink({
+    uri: constants.GRAPHQL_WEBSOCKETS_URI,
+    options: {
+      reconnect: true,
+    },
+  });
+
+  const link = split(
+    // split based on operation type
+    ({ query }) => {
+      const definition = getMainDefinition(query);
+      return (
+        definition.kind === 'OperationDefinition' &&
+        definition.operation === 'subscription'
+      );
+    },
+    wsLink,
+    httpLink
+  );
 
   return new ApolloClient({
     link: authLink.concat(httpLink),
