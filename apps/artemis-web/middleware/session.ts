b5572a9f160e04f15e471f9c0121977f8d90f93e
slowr <3428023+slowr@users.noreply.github.com>
2020-10-21 23:04:18 +0200

    fix: remove mongostore as the session persistor

--- apps/artemis-web/middleware/session.ts
+++ apps/artemis-web/middleware/session.ts
@@ -1,16 +1,7 @@
-import { session, Store, MemoryStore } from 'next-session';
-import connectMongo from 'connect-mongo';
-
-const MongoStore = connectMongo({ Store, MemoryStore });
+import { session } from 'next-session';
 
 export default function (req, res, next) {
-  const mongoStore = new MongoStore({
-    client: req.dbClient,
-    stringify: false,
-  });
-
   return session({
-    store: mongoStore,
     cookie: {
       secure: process.env.production === 'true',
     },
