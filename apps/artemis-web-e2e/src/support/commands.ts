cc8aecab61465fd6d7986f7fceb37ccb3e2d6531
Dimitrios Mavrommatis <jim.mavrommatis@gmail.com>
2020-10-22 16:15:28 +0200

    rebase (#5)
    Co-authored-by: CuriouzK0d3r <curiouz_k0d3r@protonmail.com>

--- apps/artemis-web-e2e/src/support/commands.ts
+++ apps/artemis-web-e2e/src/support/commands.ts
@@ -10,14 +10,62 @@
 // eslint-disable-next-line @typescript-eslint/no-namespace
 declare namespace Cypress {
   interface Chainable<Subject> {
-    login(email: string, password: string): void;
+    typeLogin(user: any): void;
+    login(): void;
+    register(): void;
+    typeRegister(user: any): void;
   }
 }
 //
 // -- This is a parent command --
-Cypress.Commands.add('login', (email, password) => {
-  console.log('Custom command example: Login', email, password);
+Cypress.Commands.add('typeLogin', (user) => {
+  cy.get('body').then(($body) => {
+    // synchronously query from body
+    // to find which element was created
+    if ($body.find('input[name=email]').length) {
+      cy.get('input[name=email]').type(user.email);
+
+      cy.get('input[name=password]').type(user.password);
+
+      cy.get('input[type=checkbox]').click();
+    }
+  });
 });
+
+Cypress.Commands.add('typeRegister', (user) => {
+  cy.get('body').then(($body) => {
+    // synchronously query from body
+    // to find which element was created
+    if ($body.find('#email').length) {
+      cy.get('#username').type(user.name);
+      cy.get('#email').type(user.email);
+      cy.get('#password').type(user.password);
+    }
+  });
+});
+
+Cypress.Commands.add('register', () => {
+  cy.get('body').then(($body) => {
+    // synchronously query from body
+    // to find which element was created
+    if ($body.find('#email').length) {
+      cy.get('button[type=submit]').click();
+      cy.wait(1000);
+    }
+  });
+});
+
+Cypress.Commands.add('login', () => {
+  cy.get('body').then(($body) => {
+    // synchronously query from body
+    // to find which element was created
+    if ($body.find('input[name=email]').length) {
+      cy.get('button[type=submit]').click();
+      cy.wait(1000);
+    }
+  });
+});
+
 //
 // -- This is a child command --
 // Cypress.Commands.add("drag", { prevSubject: 'element'}, (subject, options) => { ... })
