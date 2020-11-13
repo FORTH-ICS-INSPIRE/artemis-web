5168da625c724b28ae0b4f0a2de335849f56823f
Dimitrios Mavrommatis <d.mavrommatis@elsevier.com>
2020-11-13 12:45:51 +0100

    build: fixed docker setup

---
+++ apps/artemis-web/scripts/query.js
@@ -0,0 +1,43 @@
+const email = process.env.DEFAULT_EMAIL;
+const password = process.env.DEFAULT_PASS;
+const URI = `mongodb://${process.env.MONGODB_USER}:${process.env.MONGODB_PASS}@${process.env.MONGODB_HOST}:${process.env.MONGODB_PORT}`;
+
+async function isInCollection(db, name) {
+  const collections = await db.listCollections().toArray();
+  const filtered = collections.filter((collection) => collection.name === name);
+  return filtered.length > 0;
+}
+
+(async function () {
+  const { MongoClient } = require('mongodb');
+  const argon2 = require('argon2');
+  const { nanoid } = require('nanoid');
+
+  const client = new MongoClient(URI);
+  await client.connect();
+
+  const db = client.db('artemis-web');
+
+  if (!(await isInCollection(db, 'users'))) {
+    db.createCollection('users', function (err, res) {
+      if (err) throw err;
+    });
+    db.collection('users').insertOne(
+      {
+        _id: nanoid(12),
+        email: email,
+        password: await argon2.hash(password),
+        name: 'Admin',
+        lastLogin: new Date(),
+        currentLogin: new Date(),
+        role: 'admin',
+        token: '',
+      },
+      (err, res) => {
+        client.close();
+      }
+    );
+  } else {
+    client.close();
+  }
+})();
--- scripts/entrypoint.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-./wait-for.sh -h mongodb -p 27017 -t 0 -- node query.js
-yarn run start --prod
