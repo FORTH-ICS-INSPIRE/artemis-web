f201ec004ed6ad50b95d6616bd80a1a93b4d9691
CuriouzK0d3r <curiouz_k0d3r@protonmail.com>
2020-11-19 11:32:32 +0200

    feat: dynamic jsx generation for modules

--- apps/artemis-web/pages/system.tsx
+++ apps/artemis-web/pages/system.tsx
@@ -1,13 +1,26 @@
-import { Button, FormControlLabel, FormGroup, Switch } from '@material-ui/core';
+import {
+  Button,
+  FormControlLabel,
+  FormGroup,
+  Grid,
+  Switch,
+} from '@material-ui/core';
 import Head from 'next/head';
 import React, { useState } from 'react';
 import NotFoundHOC from '../components/404-hoc/404-hoc';
 
 const SystemPage = (props) => {
   const user = props.user;
-  const [isMonitorActive, setIsMonitorActive] = useState(false);
-  const [isDetectionActive, setIsDetectionActive] = useState(false);
-  const [isMitigationActive, setIsMitigationActive] = useState(false);
+  const modules = [
+    ['Monitor', false],
+    ['Detection', false],
+    ['Mitigation', false],
+  ];
+
+  const states = {};
+  modules.forEach((module) => (states[module[0].toString()] = module[1]));
+
+  const [state, setState] = useState(states);
 
   return (
     <>
@@ -31,101 +44,52 @@ const SystemPage = (props) => {
             </div>
             <div className="row">
               <div className="col-lg-1" />
-              <div className="col-lg-3">
-                <div className="card">
-                  <div className="card-header"> Monitor Module</div>
-                  <div className="card-body">
-                    <div className="row">
-                      <div className="col-lg-3">
-                        <Button
-                          variant="contained"
-                          color={isMonitorActive ? 'primary' : 'secondary'}
-                        >
-                          {isMonitorActive ? 'Active (1/1)' : 'Active (0/1)'}
-                        </Button>
-                      </div>
-                      <div className="col-lg-3">
-                        <FormGroup>
-                          <FormControlLabel
-                            control={
-                              <Switch
-                                onChange={() => {
-                                  setIsMonitorActive(!isMonitorActive);
-                                }}
-                                size="medium"
-                              />
-                            }
-                            label=""
-                          />
-                        </FormGroup>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div className="col-lg-4">
-                <div className="card">
-                  <div className="card-header">Detection Module</div>
-                  <div className="card-body">
-                    <div className="row">
-                      <div className="col-lg-3">
-                        <Button
-                          variant="contained"
-                          color={isDetectionActive ? 'primary' : 'secondary'}
-                        >
-                          {isDetectionActive ? 'Active (1/1)' : 'Active (0/1)'}
-                        </Button>
-                      </div>
-                      <div className="col-lg-3">
-                        <FormGroup>
-                          <FormControlLabel
-                            control={
-                              <Switch
-                                onChange={() => {
-                                  setIsDetectionActive(!isDetectionActive);
-                                }}
-                                size="medium"
-                              />
-                            }
-                            label=""
-                          />
-                        </FormGroup>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div className="col-lg-3">
-                <div className="card">
-                  <div className="card-header">Mitigation Module</div>
-                  <div className="card-body">
-                    <div className="row">
-                      <div className="col-lg-3">
-                        <Button
-                          variant="contained"
-                          color={isMitigationActive ? 'primary' : 'secondary'}
-                        >
-                          {isMitigationActive ? 'Active (1/1)' : 'Active (0/1)'}
-                        </Button>
-                      </div>
-                      <div className="col-lg-3">
-                        <FormGroup>
-                          <FormControlLabel
-                            control={
-                              <Switch
-                                onChange={() => {
-                                  setIsMitigationActive(!isMitigationActive);
-                                }}
-                                size="medium"
-                              />
-                            }
-                            label=""
-                          />
-                        </FormGroup>
-                      </div>
-                    </div>
-                  </div>
-                </div>
+              <div className="col-lg-10">
+                <Grid container spacing={3}>
+                  {Object.keys(state).map((module) => {
+                    return (
+                      <Grid item xs={4}>
+                        <div className="card">
+                          <div className="card-header"> {module} Module</div>
+                          <div className="card-body">
+                            <div className="row">
+                              <div className="col-lg-3">
+                                <Button
+                                  variant="contained"
+                                  color={
+                                    state[module] ? 'primary' : 'secondary'
+                                  }
+                                >
+                                  {state[module]
+                                    ? 'Active (1/1)'
+                                    : 'Active (0/1)'}
+                                </Button>
+                              </div>
+                              <div className="col-lg-3">
+                                <FormGroup>
+                                  <FormControlLabel
+                                    control={
+                                      <Switch
+                                        onChange={() => {
+                                          setState((prevState) => ({
+                                            ...prevState,
+                                            [module]: !state[module],
+                                          }));
+                                        }}
+                                        size="medium"
+                                      />
+                                    }
+                                    label=""
+                                  />
+                                </FormGroup>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </Grid>
+                    );
+                  })}
+                </Grid>
               </div>
             </div>
           </div>
