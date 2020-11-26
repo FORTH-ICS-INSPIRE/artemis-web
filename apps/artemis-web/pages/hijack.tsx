a0e0db5ba6f34e2056f40e9b5ccadffe33fbf945
CuriouzK0d3r <curiouz_k0d3r@protonmail.com>
2020-11-26 18:24:38 +0200

    fix: reduce complexity

--- apps/artemis-web/pages/hijack.tsx
+++ apps/artemis-web/pages/hijack.tsx
@@ -29,13 +29,13 @@ const useStyles = makeStyles((theme) => ({
 
 const ViewHijackPage = (props) => {
   const [isLive, setIsLive] = useState(true);
+  const isDevelopment = process.env.NODE_ENV === 'development';
+  const isBrowser = typeof window !== 'undefined';
 
-  if (process.env.NODE_ENV === 'development') {
-    if (typeof window !== 'undefined') {
-      // eslint-disable-next-line @typescript-eslint/no-var-requires
-      const { worker } = require('../utils/mock-sw/browser');
-      worker.start();
-    }
+  if (isDevelopment && isBrowser) {
+    // eslint-disable-next-line @typescript-eslint/no-var-requires
+    const { worker } = require('../utils/mock-sw/browser');
+    worker.start();
   }
 
   const statuses = {
