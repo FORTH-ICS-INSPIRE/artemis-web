6429ab75afd0aa08cd9aea48d2f7cdd26e69fa24
kornilak <curiouz_k0d3r@protonmail.com>
2020-12-30 18:11:54 +0200

    fix: failed test

--- apps/artemis-web/components/bgp-table/bgp-table.spec.tsx
+++ apps/artemis-web/components/bgp-table/bgp-table.spec.tsx
@@ -3,11 +3,42 @@ import { render } from '@testing-library/react';
 import { MockedProvider } from '@apollo/client/testing';
 
 import BGPTable from './bgp-table';
+import { QueryGenerator } from '../../libs/graphql';
 
 describe('BGPTable', () => {
   it('should render successfully', () => {
+    const generator = new QueryGenerator('bgpUpdates', false, {});
+    const mocks = [
+      {
+        request: {
+          operationName: 'bgpupdates',
+          query: generator.getQuery(),
+        },
+        result: {
+          data: {
+            view_bgpupdates: [
+              {
+                prefix: '192.67.249.0/24',
+                origin_as: 8522,
+                peer_asn: 208627,
+                as_path: [208627, 43350, 21320, 5408, 8522],
+                service: 'ripe-ris|rrc03',
+                type: 'A',
+                communities: [],
+                timestamp: new Date(),
+                hijack_key: [],
+                handled: true,
+                matched_prefix: '192.67.249.0/24',
+                orig_path: null,
+              },
+            ],
+          },
+        },
+      },
+    ];
+
     const { baseElement } = render(
-      <MockedProvider>
+      <MockedProvider mocks={mocks}>
         <BGPTable
           filter={0}
           isLive={false}
