7429121b54a01c7d468666164e2caad803918b01
kornilak <curiouz_k0d3r@protonmail.com>
2021-03-03 11:57:49 +0200

    test: disabling some tests

--- apps/artemis-web/components/system-configuration/system-configuration.spec.tsx
+++ apps/artemis-web/components/system-configuration/system-configuration.spec.tsx
@@ -4,23 +4,23 @@ import SystemConfigurationComponent from './system-configuration';
 
 describe('ConfigComparisonComponent', () => {
   it('should render successfully', () => {
-    // document.createRange = function () {
-    //   return {
-    //     setEnd: function () {
-    //       return true;
-    //     },
-    //     setStart: function () {
-    //       return true;
-    //     },
-    //     getBoundingClientRect: function () {
-    //       return { right: 0 };
-    //     },
-    //   };
-    // };
+    document.createRange = function () {
+      return {
+        setEnd: function () {
+          return true;
+        },
+        setStart: function () {
+          return true;
+        },
+        getBoundingClientRect: function () {
+          return { right: 0 };
+        },
+      };
+    };
 
-    // const { baseElement } = render(
-    //   <SystemConfigurationComponent CONFIG_DATA={null} />
-    // );
-    // expect(baseElement).toBeTruthy();
+    const { baseElement } = render(
+      <SystemConfigurationComponent CONFIG_DATA={null} />
+    );
+    expect(baseElement).toBeTruthy();
   });
 });
