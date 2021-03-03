87bccfb71b026bad88dd3c135b7e72c95f2f3c02
kornilak <curiouz_k0d3r@protonmail.com>
2021-03-03 11:43:59 +0200

    test: disabling some tests

--- apps/artemis-web/components/bgp-table/bgp-table.spec.tsx
+++ apps/artemis-web/components/bgp-table/bgp-table.spec.tsx
@@ -8,88 +8,88 @@ import { gql } from '@apollo/client';
 
 describe('BGPTable', () => {
   it('should render successfully', () => {
-    // const generator2 = new QueryGenerator('bgpCount', false, {});
+    const generator2 = new QueryGenerator('bgpCount', false, {});
 
-    // const mocks = [
-    //   {
-    //     request: {
-    //       operationName: 'bgpupdates',
-    //       query: gql`
-    //         query bgpupdates($offset: Int!, $limit: Int!) {
-    //           view_bgpupdates(
-    //             limit: $limit
-    //             offset: $offset
-    //             order_by: { timestamp: desc }
-    //           ) {
-    //             prefix
-    //             origin_as
-    //             peer_asn
-    //             as_path
-    //             service
-    //             type
-    //             communities
-    //             timestamp
-    //             hijack_key
-    //             handled
-    //             matched_prefix
-    //             orig_path
-    //             __typename
-    //           }
-    //         }
-    //       `,
-    //       variables: { offset: 0, limit: 10 },
-    //     },
-    //     result: {
-    //       data: {
-    //         view_bgpupdates: [
-    //           {
-    //             prefix: '192.67.249.0/24',
-    //             origin_as: 8522,
-    //             peer_asn: 208627,
-    //             as_path: [208627, 43350, 21320, 5408, 8522],
-    //             service: 'ripe-ris|rrc03',
-    //             type: 'A',
-    //             communities: [],
-    //             timestamp: new Date(),
-    //             hijack_key: [],
-    //             handled: true,
-    //             matched_prefix: '192.67.249.0/24',
-    //             orig_path: null,
-    //           },
-    //         ],
-    //       },
-    //     },
-    //   },
-    //   {
-    //     request: {
-    //       operationName: 'getLiveTableCount',
-    //       query: generator2.getQuery(),
-    //     },
-    //     result: {
-    //       data: {
-    //         count_data: {
-    //           aggregate: {
-    //             count: 25,
-    //             __typename: 'view_hijacks_aggregate_fields',
-    //           },
-    //           __typename: 'view_hijacks_aggregate',
-    //         },
-    //       },
-    //     },
-    //   },
-    // ];
+    const mocks = [
+      {
+        request: {
+          operationName: 'bgpupdates',
+          query: gql`
+            query bgpupdates($offset: Int!, $limit: Int!) {
+              view_bgpupdates(
+                limit: $limit
+                offset: $offset
+                order_by: { timestamp: desc }
+              ) {
+                prefix
+                origin_as
+                peer_asn
+                as_path
+                service
+                type
+                communities
+                timestamp
+                hijack_key
+                handled
+                matched_prefix
+                orig_path
+                __typename
+              }
+            }
+          `,
+          variables: { offset: 0, limit: 10 },
+        },
+        result: {
+          data: {
+            view_bgpupdates: [
+              {
+                prefix: '192.67.249.0/24',
+                origin_as: 8522,
+                peer_asn: 208627,
+                as_path: [208627, 43350, 21320, 5408, 8522],
+                service: 'ripe-ris|rrc03',
+                type: 'A',
+                communities: [],
+                timestamp: new Date(),
+                hijack_key: [],
+                handled: true,
+                matched_prefix: '192.67.249.0/24',
+                orig_path: null,
+              },
+            ],
+          },
+        },
+      },
+      {
+        request: {
+          operationName: 'getLiveTableCount',
+          query: generator2.getQuery(),
+        },
+        result: {
+          data: {
+            count_data: {
+              aggregate: {
+                count: 25,
+                __typename: 'view_hijacks_aggregate_fields',
+              },
+              __typename: 'view_hijacks_aggregate',
+            },
+          },
+        },
+      },
+    ];
 
-    // const { baseElement } = render(
-    //   <MockedProvider mocks={mocks}>
-    //     <BGPTable
-    //       filter={0}
-    //       isLive={false}
-    //       setFilteredBgpData={(data) => {
-    //         return;
-    //       }}
-    //     />
-    //   </MockedProvider>
-    // );
-    // expect(baseElement).toBeTruthy();
+    const { baseElement } = render(
+      <MockedProvider mocks={mocks}>
+        <BGPTable
+          filter={0}
+          isLive={false}
+          setFilteredBgpData={(data) => {
+            return;
+          }}
+        />
+      </MockedProvider>
+    );
+    expect(baseElement).toBeTruthy();
   });
 });
--- apps/artemis-web/components/config-comparison/config-comparison.spec.tsx
+++ apps/artemis-web/components/config-comparison/config-comparison.spec.tsx
@@ -4,21 +4,21 @@ import ConfigComparisonComponent from './config-comparison';
 
 describe('ConfigComparisonComponent', () => {
   it('should render successfully', () => {
-    // document.createRange = function () {
-    //   return {
-    //     setEnd: function () {
-    //       return true;
-    //     },
-    //     setStart: function () {
-    //       return true;
-    //     },
-    //     getBoundingClientRect: function () {
-    //       return { right: 0 };
-    //     },
-    //   };
-    // };
+    document.createRange = function () {
+      return {
+        setEnd: function () {
+          return true;
+        },
+        setStart: function () {
+          return true;
+        },
+        getBoundingClientRect: function () {
+          return { right: 0 };
+        },
+      };
+    };
 
-    // const { baseElement } = render(<ConfigComparisonComponent />);
-    // expect(baseElement).toBeTruthy();
+    const { baseElement } = render(<ConfigComparisonComponent />);
+    expect(baseElement).toBeTruthy();
   });
 });
