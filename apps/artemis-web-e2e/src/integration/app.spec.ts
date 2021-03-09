21f8d7ef0e194301c9f916f6971b7e826ec017a1
kornilak <curiouz_k0d3r@protonmail.com>
2021-03-09 19:20:30 +0200

    test: split cypress

--- apps/artemis-web-e2e/src/integration/app.spec.ts
+++ apps/artemis-web-e2e/src/integration/app.spec.ts
@@ -23,31 +23,39 @@ describe('artemis-web', () => {
     // Custom command example, see `../support/commands.ts` file
     cy.get('#logout', { timeout: 2000 }).click({ force: true });
     cy.wait(2000);
-    cy.get('h1').should('have.text', 'Login');
+    cy.get('h1').should('have.text', 'Sign In');
   });
 
   it('logs in', () => {
     cy.visit('/login');
     cy.wait(2000);
-    cy.get('h1').should('have.text', 'Login');
+    cy.get('h1').should('have.text', 'Sign In');
     cy.typeLogin({ email: newEmail, password: newPass });
     cy.login();
   });
 
-  it('after login > Dashboard', () => {
+
+  it('change password', () => {
     // Custom command example, see `../support/commands.ts` file
-    cy.visit('/dashboard');
+    cy.visit('/password_change');
     cy.wait(2000);
-    cy.waitFor('h1');
-    cy.get('h1').should('have.text', 'Dashboard');
-    cy.get('#modules').find('tr').its('length').should('be.gt', 0);
+    cy.typeChangePass({
+      old_pass: newPass,
+      new_pass: '1234',
+      repeat_pass: '1234',
+    });
+    cy.get('#submit').click();
+    cy.wait(1000);
+    cy.get('#password_change_form').contains('has been updated');
   });
 
-  // it('diplays modules', () => {
-  //   cy.visit('/dashboard');
-  //   cy.wait(2000);
-  //   // Custom command example, see `../support/commands.ts` file
-  //   cy.get('#modules').find('tr').its('length').should('be.gt', 0);
-  // });
-
+  it('logs in', () => {
+    cy.visit('/login');
+    cy.wait(2000);
+    cy.get('h1').should('have.text', 'Sign In');
+    cy.typeLogin({ email: newEmail, password: '1234' });
+    cy.login();
   });
+
+
+});
--- apps/artemis-web-e2e/src/integration/app2.spec.ts
+++ apps/artemis-web-e2e/src/integration/app2.spec.ts
@@ -7,45 +7,31 @@ describe('artemis-web', () => {
   beforeEach(() => {
     Cypress.Cookies.preserveOnce('remember_me', 'sid', 'access_token');
   });
-  
+
   it('logs in with ldap', () => {
     cy.visit('/login');
     cy.wait(2000);
-    cy.get('h1').should('have.text', 'Login');
+    cy.get('h1').should('have.text', 'Sign In');
     cy.typeLogin({ email: 'hermes@planetexpress.com', password: 'hermes' });
     cy.loginLDAP();
     cy.waitFor('h1');
     cy.get('h1').should('have.text', 'Dashboard');
   });
 
-  it('change password', () => {
-    // Custom command example, see `../support/commands.ts` file
-    cy.visit('/password_change');
-    cy.wait(2000);
-    cy.typeChangePass({
-      old_pass: newPass,
-      new_pass: '1234',
-      repeat_pass: '1234',
-    });
-    cy.get('#submit').click();
-    cy.wait(1000);
-    cy.get('#password_change_form').contains('has been updated');
-  });
 
-  it('logs out', () => {
+  it('after login > Dashboard', () => {
     // Custom command example, see `../support/commands.ts` file
-    cy.get('#logout', { timeout: 4000 }).click({ force: true });
+    cy.visit('/dashboard');
     cy.wait(2000);
-    cy.get('h1').should('have.text', 'Login');
-  });
-
-  it('logs in with ldap', () => {
-    cy.visit('/login');
-    cy.wait(2000);
-    cy.get('h1').should('have.text', 'Login');
-    cy.typeLogin({ email: 'hermes@planetexpress.com', password: 'hermes' });
-    cy.loginLDAP();
     cy.waitFor('h1');
     cy.get('h1').should('have.text', 'Dashboard');
+    cy.get('#modules').find('tr').its('length').should('be.gt', 0);
+  });
+
+  it('diplays modules', () => {
+    cy.visit('/dashboard');
+    cy.wait(2000);
+    // Custom command example, see `../support/commands.ts` file
+    cy.get('#modules').find('tr').its('length').should('be.gt', 0);
   });
 });
