a5d4326500098ef913660da8eac27f03e1a6a5bb
kornilak <curiouz_k0d3r@protonmail.com>
2021-03-09 22:31:50 +0200

    test: split cypress

--- apps/artemis-web-e2e/src/integration/app2.spec.ts
+++ apps/artemis-web-e2e/src/integration/app2.spec.ts
@@ -16,6 +16,7 @@ describe('artemis-web', () => {
     cy.loginLDAP();
     cy.waitFor('h1');
     cy.get('h1').should('have.text', 'Dashboard');
+    cy.wait(2000);
     cy.get('#modules').find('tr').its('length').should('be.gt', 0);
 
   });
