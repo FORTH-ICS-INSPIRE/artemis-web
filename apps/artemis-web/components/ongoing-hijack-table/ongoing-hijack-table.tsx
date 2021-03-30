a8178f56553f37cb11675c91d2a475349d74f3e0
CuriouzK0d3r <curiouz_k0d3r@protonmail.com>
2021-03-30 12:38:26 +0300

    fix: move filters on top

--- apps/artemis-web/components/ongoing-hijack-table/ongoing-hijack-table.tsx
+++ apps/artemis-web/components/ongoing-hijack-table/ongoing-hijack-table.tsx
@@ -560,7 +560,7 @@ const OngoingHijackTableComponent = (props) => {
               expandRow={getExpandRow(expandState)}
               filter={filterFactory()}
               onTableChange={handleTableChange}
-              filterPosition="bottom"
+              filterPosition="top"
               striped
               hover
               noDataIndication={() => {
