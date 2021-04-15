b87483cf5d32ab8deb3070fe8610e2d7d3354c97
CuriouzK0d3r <curiouz_k0d3r@protonmail.com>
2021-04-15 11:40:54 +0300

    fix: security headers

--- apps/artemis-web/next.config.js
+++ apps/artemis-web/next.config.js
@@ -3,6 +3,19 @@ const { createSecureHeaders } = require("next-secure-headers");
 
 module.exports = {
   async headers() {
-    return [{ source: "/(.*)", headers: createSecureHeaders() }];
+    return [{
+      source: "/(.*)",
+      headers: createSecureHeaders({
+        contentSecurityPolicy: {
+          directives: {
+            defaultSrc: "'self'",
+            styleSrc: ["'self'", "https://cdn.jsdelivr.net", "https://code.jquery.com"],
+            scriptSrc: ["'self'", "https://cdn.jsdelivr.net", "https://code.jquery.com"]
+          },
+        },
+        forceHTTPSRedirect: [true, { maxAge: 60 * 60 * 24 * 4, includeSubDomains: true }],
+        referrerPolicy: "same-origin",
+      }),
+    }];
   },
 };
