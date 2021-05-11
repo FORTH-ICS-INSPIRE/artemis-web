b47cc9c87da109252641a3397c7dcc9f15115ec5
CuriouzK0d3r <curiouz_k0d3r@protonmail.com>
2021-05-11 21:41:32 +0300

    fix: minor bug

--- apps/artemis-web/libs/graphql.ts
+++ apps/artemis-web/libs/graphql.ts
@@ -28,7 +28,7 @@ const createApolloClient = () => {
   const httpLink =
     typeof window !== 'undefined'
       ? createHttpLink({
-        uri: `https://demo.artemis-pc.duckdns.org/api/graphql`,
+        uri: `https://${window.location.hostname}/api/graphql`,
         useGETForQueries: false,
       })
       : null;
@@ -49,7 +49,7 @@ const createApolloClient = () => {
   const wsLink =
     typeof window !== 'undefined'
       ? new WebSocketLink({
-        uri: `wss://demo.artemis-pc.duckdns.org/api/graphql`,
+        uri: `wss://${window.location.hostname}/api/graphql`,
         options: {
           reconnect: true,
           lazy: true,
