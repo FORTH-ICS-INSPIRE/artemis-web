b47cc55771bd2335ac77ed4b7d8e169d57a90ae1
CuriouzK0d3r <curiouz_k0d3r@protonmail.com>
2021-06-25 16:37:25 +0300

    fix: code smells

--- apps/artemis-web/components/ongoing-hijack-table/ongoing-hijack-table.tsx
+++ apps/artemis-web/components/ongoing-hijack-table/ongoing-hijack-table.tsx
@@ -21,9 +21,7 @@ import {
   getSortCaret,
   isObjectEmpty,
   shallSubscribe,
-  expandedColumnHeaderComponent,
   getExactMatchFilter,
-  expandColumnComponent,
   genTooltip,
   compareObjects,
   isNumeric,
@@ -31,133 +29,6 @@ import {
 import ErrorBoundary from '../error-boundary/error-boundary';
 import ExportJSON from '../export-json/export-json';
 import Tooltip from '../tooltip/tooltip';
-
-const getExpandRow = (expandState: any) => {
-  return {
-    showExpandColumn: true,
-    expandByColumnOnly: true,
-    expandColumnPosition: 'right',
-    expanded: expandState,
-    expandColumnRenderer: expandColumnComponent,
-    expandHeaderColumnRenderer: expandedColumnHeaderComponent,
-    renderer: (row: any) => {
-      return (
-        <table>
-          <thead></thead>
-          <tbody>
-            <tr>
-              <td>
-                <b>
-                  <div data-tip data-for={'hprefix_exp'}>
-                    {<span>{'Hijacked Prefix'}</span>}
-                  </div>
-                  <ReactTooltip html={true} id={'hprefix_exp'}>
-                    {'The IPv4/IPv6 prefix that was hijacked.'}
-                  </ReactTooltip>
-                </b>
-              </td>
-              <td>{row.prefix.toString()}</td>
-            </tr>
-            <tr>
-              <td>
-                <b>
-                  <div data-tip data-for={'mprefix_exp'}>
-                    {<span>{'Hijacked Prefix'}</span>}
-                  </div>
-                  <ReactTooltip html={true} id={'mprefix_exp'}>
-                    {
-                      'The configured IPv4/IPv6 prefix that matched the hijacked prefix.'
-                    }
-                  </ReactTooltip>
-                </b>
-              </td>
-              <td>{row.configured_prefix.toString()}</td>
-            </tr>
-            <tr>
-              <td>
-                <b>
-                  <div data-tip data-for={'type_exp'}>
-                    {<span>{'Type'}</span>}
-                  </div>
-                  <ReactTooltip html={true} id={'type_exp'}>
-                    {
-                      'The type of the hijack in 4 dimensions: prefix|path|data plane|policy.'
-                    }
-                  </ReactTooltip>
-                </b>
-              </td>
-              <td>{row.type}</td>
-            </tr>
-            <tr>
-              <td>
-                <b>
-                  <div data-tip data-for={'rpki_exp'}>
-                    {<span>{'RPKI'}</span>}
-                  </div>
-                  <ReactTooltip html={true} id={'rpki_exp'}>
-                    {'The RPKI status of the hijacked prefix.'}
-                  </ReactTooltip>
-                </b>
-              </td>
-              <td>{row.rpki_status.toString()}</td>
-            </tr>
-            <tr>
-              <td>
-                <b>
-                  <div data-tip data-for={'seen_exp'}>
-                    {<span>{'# Peers Seen'}</span>}
-                  </div>
-                  <ReactTooltip html={true} id={'seen_exp'}>
-                    {
-                      'Number of peers/monitors (i.e., ASNs)<br>that have seen hijack updates.'
-                    }
-                  </ReactTooltip>
-                </b>
-              </td>
-              <td>{row.num_peers_seen.toString()}</td>
-            </tr>
-            <tr>
-              <td>
-                <b>
-                  <div data-tip data-for={'inf_exp'}>
-                    {<span>{'# ASes Infected'}</span>}
-                  </div>
-                  <ReactTooltip html={true} id={'inf_exp'}>
-                    {
-                      'Number of infected ASes that seem to<br>route traffic towards the hijacker AS.<br>Note that this is an experimental field.'
-                    }
-                  </ReactTooltip>
-                </b>
-              </td>
-              <td>{row.num_asns_inf.toString()}</td>
-            </tr>
-            <tr>
-              <td>
-                <b>
-                  <div data-tip data-for={'ack_exp'}>
-                    {<span>{'ACK'}</span>}
-                  </div>
-                  <ReactTooltip html={true} id={'ack_exp'}>
-                    {
-                      'Whether the user has acknowledged/confirmed the hijack as a true positive.<br>If the resolve|mitigate buttons are pressed this<br>is automatically set to True (default value: False).'
-                    }
-                  </ReactTooltip>
-                </b>
-              </td>
-              <td>
-                {row.resolved || row.under_mitigation ? (
-                  <img alt="" src="./handled.png" />
-                ) : (
-                  <img alt="" src="./unhadled.png" />
-                )}
-              </td>
-            </tr>
-          </tbody>
-        </table>
-      );
-    },
-  };
-};
 
 const getColumns = (stateValues) => [
   {
@@ -383,7 +254,6 @@ const OngoingHijackTableComponent = (props: any): ReactElement => {
   const [page, setPage] = useState(0);
   const [sizePerPage, setSizePerPage] = useState(10);
   const [columnFilter, setColumnFilter] = useState({});
-  const expandState = [];
   const [limitState, setLimitState] = useState(10);
   const [offsetState, setOffsetState] = useState(0);
   const [sortState, setSortState] = useState('desc');
