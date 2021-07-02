b54c1898ba9ec6f2e680110995142d8b49d270dd
CuriouzK0d3r <curiouz_k0d3r@protonmail.com>
2021-07-02 09:05:32 +0300

    feat: add tailwind

--- apps/artemis-web/pages/_app.tsx
+++ apps/artemis-web/pages/_app.tsx
@@ -4,6 +4,7 @@ import { ApolloProvider } from '@apollo/client';
 import { useApollo } from '../libs/graphql';
 import Layout from '../components/layout/layout';
 import TooltipContext from '../context/tooltip-context';
+import 'tailwindcss/tailwind.css';
 
 const useStateWithLocalStorage = (localStorageKey) => {
   const [value, setValue] = useState(
