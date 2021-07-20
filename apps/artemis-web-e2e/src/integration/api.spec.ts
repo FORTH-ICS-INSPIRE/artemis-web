5820dc60f960e975244bb88be5d3726f4c8cb1e1
CuriouzK0d3r <curiouz_k0d3r@protonmail.com>
2021-07-20 19:03:46 +0300

    feat: api tests

--- apps/artemis-web-e2e/src/integration/api.spec.ts
+++ apps/artemis-web-e2e/src/integration/api.spec.ts
@@ -1,14 +1,72 @@
 import Chance from 'chance';
 
+let newEmail;
+let newPass;
 
 
-describe('artemis-web', () => {
-    it('[API] login', () => {
+describe('artemis-web-api', () => {
+    it('[API] signup', () => {
+        cy.visit('/');
+        const chance = new Chance();
+        newEmail = chance.email();
+        newPass = chance.string({ length: 5 });
+        const name = chance.first();
+
+        cy.request({
+            method: 'POST',
+            url: '/api/auth/signup',
+            headers: {
+                'x-artemis-api-key': Cypress.env('API_KEY')
+            },
+            body: {
+                "email": newEmail,
+                "password": newPass,
+                "name": name,
+            }
+        }).then((response) => {
+            expect(response.status, '200');
+
+        });
+    });
+
+
+    it('[API] credentials login', () => {
         cy.visit('/');
         cy.request({
             method: 'POST',
-            url: '/api/auth/credentials',
-            body: {}
+            url: '/api/auth/login/credentials',
+            headers: {
+                'x-artemis-api-key': Cypress.env('API_KEY')
+            },
+            body: {
+                "email": newEmail,
+                "password": newPass,
+                "rememberMe": false,
+                "_csrf": ""
+            }
+        }).then((response) => {
+            expect(response.body).to.have.property('user');
+
+        });
+    });
+
+    it('[API] LDAP login', () => {
+        cy.visit('/');
+        cy.request({
+            method: 'POST',
+            url: '/api/auth/login/ldap',
+            headers: {
+                'x-artemis-api-key': Cypress.env('API_KEY')
+            },
+            body: {
+                "email": "hermes@planetexpress.com",
+                "password": "hermes",
+                "rememberMe": false,
+                "_csrf": ""
+            }
+        }).then((response) => {
+            expect(response.body).to.have.property('user');
+
         });
     });
 });
