6514beb09e06bcb81c138d47aac1c7968a98f800
CuriouzK0d3r <curiouz_k0d3r@protonmail.com>
2021-08-24 10:19:46 +0000

    fix: more tests

--- apps/artemis-web/components/status-table/stats-table.spec.tsx
+++ apps/artemis-web/components/status-table/stats-table.spec.tsx
@@ -1,19 +1,21 @@
 import React from 'react';
-import { render } from '@testing-library/react';
-
+import Enzyme, { mount } from 'enzyme';
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
 import StatusTable from './status-table';
+
+Enzyme.configure({ adapter: new Adapter() });
 
 describe('StatsTable', () => {
   it('should render successfully', () => {
-    // const { baseElement } = render(
-    //   <StatusTable
-    //     data={{
-    //       view_processes: [
-    //         { name: 'test', running: true, timestamp: Date.now() },
-    //       ],
-    //     }}
-    //   />
-    // );
-    // expect(baseElement).toBeTruthy();
+    const element = mount(
+      <StatusTable
+        data={{
+          view_processes: [
+            { name: 'test', running: true, timestamp: Date.now() },
+          ],
+        }}
+      />
+    );
+    expect(element.text()).toContain('Monitor (tap) Microservices');
   });
 });
--- apps/artemis-web/components/user-list/user-list.spec.tsx
+++ apps/artemis-web/components/user-list/user-list.spec.tsx
@@ -1,11 +1,21 @@
 import React from 'react';
-import { render } from '@testing-library/react';
-
+import Enzyme, { mount } from 'enzyme';
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
 import UserListComponent from './user-list';
+
+Enzyme.configure({ adapter: new Adapter() });
+
 
 describe('UserList', () => {
   it('should render successfully', () => {
-    const { baseElement } = render(<UserListComponent data={[]} />);
-    expect(baseElement).toBeTruthy();
+    const element = mount(<UserListComponent data={[{
+      _id: 1,
+      name: 'test',
+      email: 'test@test.com',
+      role: 'user',
+      lastLogin: ''
+    }]} />);
+
+    expect(element.text()).toContain('test@test.com');
   });
 });
