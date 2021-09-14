69f51e80d1aaf55f366601a71eb42d8f8a30af54
CuriouzK0d3r <curiouz_k0d3r@protonmail.com>
2021-09-14 11:15:32 +0300

    fix: upgrade

--- apps/artemis-web/components/login/login.spec.tsx
+++ apps/artemis-web/components/login/login.spec.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import Enzyme, { mount } from 'enzyme';
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
 
 import Login from './login';
+import { Button, TextField } from '@material-ui/core';
+
+Enzyme.configure({ adapter: new Adapter() });
 
 describe('Login', () => {
   it('should render successfully', () => {
-    const { baseElement } = render(<Login />);
-    expect(baseElement).toBeTruthy();
+    const element = mount(<Login />);
+    expect(element.text()).toContain('Sign In');
+    expect(element.find(TextField)).toHaveLength(2);
+    expect(element.find(Button)).toHaveLength(2);
   });
 });
