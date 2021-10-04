6540c8a6effe14ad0a13868a0317f1aa38a6d66c
CuriouzK0d3r <curiouz_k0d3r@protonmail.com>
2021-10-04 17:24:14 +0300

    fix: minor code smells

--- apps/artemis-web/components/login/login.tsx
+++ apps/artemis-web/components/login/login.tsx
@@ -13,7 +13,7 @@ import { theme, useStyles } from '../../utils/styles';
 import { useRouter } from 'next/router';
 import React, { useEffect, useState } from 'react';
 
-const Login = (props) => {
+const Login = (props: any) => {
   const [errorMsg, setErrorMsg] = useState('');
   const [formData, setFormData] = useState({
     email: '',
