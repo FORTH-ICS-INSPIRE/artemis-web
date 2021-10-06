b0705113f5a5144717850d8489cdc5bda826fd59
CuriouzK0d3r <curiouz_k0d3r@protonmail.com>
2021-10-06 09:19:43 +0000

    fix: lint errors

--- apps/artemis-web/next.config.js
+++ apps/artemis-web/next.config.js
@@ -1,13 +1,7 @@
 // /next.config.js
-const { createSecureHeaders } = require('next-secure-headers');
-
-
 require("dotenv").config({
   path: `.env`,
 })
-
-const webpack = require('webpack')
-const nodeExternals = require('webpack-node-externals');
 
 module.exports = {
   poweredByHeader: false,
