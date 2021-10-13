cca7209625818edc845236375ce29315ba32cbdf
CuriouzK0d3r <curiouz_k0d3r@protonmail.com>
2021-10-13 13:09:40 +0300

    fix: upgrade react-media

--- apps/artemis-web/components/header/header.tsx
+++ apps/artemis-web/components/header/header.tsx
@@ -1,5 +1,6 @@
+import { GLOBAL_MEDIA_QUERIES } from '../../utils/token';
 import React from 'react';
-import { useMedia } from 'react-media';
+import Media from 'react-media';
 import { useJWT } from '../../utils/hooks/use-jwt';
 import DesktopHeader from '../desktop-header/desktop-header';
 import MobileHeader from '../mobile-header/mobile-header';
@@ -7,26 +8,27 @@ import MobileHeader from '../mobile-header/mobile-header';
 const Header = (props) => {
   const [user, _] = useJWT();
 
-  const GLOBAL_MEDIA_QUERIES = {
-    pc: '(min-width: 700px)',
-    mobile: '(max-width: 700px)',
-  };
-  const matches = useMedia({ queries: GLOBAL_MEDIA_QUERIES });
-
   return (
-    <nav
-      className="navbar navbar-expand-md navbar-dark fixed-top bg-dark"
-      style={{
-        marginBottom: '20px',
-        padding: matches.mobile ? '.5rem 0rem' : '.5rem 1rem',
-      }}
-    >
-      {matches.mobile ? (
-        <MobileHeader user={user} {...props} />
-      ) : (
-        <DesktopHeader user={user} {...props} />
+    <Media queries={GLOBAL_MEDIA_QUERIES}>
+      {matches => (
+        <nav
+          className="navbar navbar-expand-md navbar-dark fixed-top bg-dark"
+          style={{
+            marginBottom: '20px',
+            padding: matches.mobile ? '.5rem 0rem' : '.5rem 1rem',
+          }}
+        >
+
+          {
+            matches.mobile ? (
+              <MobileHeader user={user} {...props} />
+            ) : (
+              <DesktopHeader user={user} {...props} />
+            )
+          }
+        </nav>
       )}
-    </nav>
+    </Media>
   );
 };
 
