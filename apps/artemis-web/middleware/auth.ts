50d5d3a84845a46b7ecc8601b5dec9c4271a10e7
CuriouzK0d3r <curiouz_k0d3r@protonmail.com>
2021-10-14 19:53:08 +0300

    fix: upgrade

--- apps/artemis-web/middleware/auth.ts
+++ apps/artemis-web/middleware/auth.ts
@@ -1,19 +1,29 @@
 import nc from 'next-connect';
 import passport from '../libs/passport';
 import database from './database';
-import { session } from 'next-session';
+import nextSession from "next-session";
+const getSession = nextSession({
+  cookie: {
+    maxAge: parseInt(process.env.SESSION_TIMEOUT, 10),
+    secure:
+      process.env.NODE_ENV === 'production' &&
+      process.env.TESTING === 'false',
+  },
+});
+
+async function session(req, res, next) {
+  try {
+    await getSession(req, res);
+    next();
+  } catch (err) {
+    next(err);
+  }
+}
 
 const auth = nc()
   .use(database)
   .use(
-    session({
-      cookie: {
-        maxAge: parseInt(process.env.SESSION_TIMEOUT, 10),
-        secure:
-          process.env.NODE_ENV === 'production' &&
-          process.env.TESTING === 'false',
-      },
-    })
+    session
   )
   .use(passport.initialize())
   .use(passport.session())
