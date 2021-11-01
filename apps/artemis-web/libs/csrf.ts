cba9d5ae283921e84ccae36e1ca86870d8eba1eb
CuriouzK0d3r <curiouz_k0d3r@protonmail.com>
2021-11-01 15:55:46 +0200

    fix: csrf bug

--- apps/artemis-web/libs/csrf.ts
+++ apps/artemis-web/libs/csrf.ts
@@ -3,6 +3,7 @@ import { nextCsrf } from './next-csrf';
 
 const options = {
   secret: process.env.CSRF_SECRET, // Long, randomly-generated, unique, and unpredictable value
+  csrfSecret: process.env.CSRF_SECRET,
 };
 
 export const { csrf, setup, csrfToken } = nextCsrf(options);
