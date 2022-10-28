cc992dc3e152b11413ea44d377b78515be9e0482
CuriouzK0d3r <curiouz_k0d3r@protonmail.com>
2022-10-28 19:17:09 +0300

    fix: hijacker AS label + entrypoint minor fix

--- apps/artemis-web/components/ongoing-hijack-table/ongoing-hijack-table.tsx
+++ apps/artemis-web/components/ongoing-hijack-table/ongoing-hijack-table.tsx
@@ -118,8 +118,8 @@ const getColumns = (stateValues) => [
         'as_title',
         'The AS that is potentially responsible for the hijack.</br>Note that this is an experimental field.'
       ),
-    text: 'Hijacked AS',
-    filter: getExactMatchFilter(stateValues['hijack_as'], 'Hijacked AS'),
+    text: 'Hijacker AS',
+    filter: getExactMatchFilter(stateValues['hijack_as'], 'Hijacker AS'),
   },
   {
     dataField: 'rpki_status',
@@ -356,8 +356,8 @@ const OngoingHijackTableComponent = (props: any): ReactElement => {
             key={i}
             value={option.text}
             className={`btn ${currSizePerPage === `${option.page}`
-                ? 'btn-secondary'
-                : 'btn-warning'
+              ? 'btn-secondary'
+              : 'btn-warning'
               }`}
           >
             {option.text}
