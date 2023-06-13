b1e06e3cbcb1692fd4a67db693db43b0ad11744f
CuriouzK0d3r <curiouz_k0d3r@protonmail.com>
2023-06-13 11:51:07 +0300

    fix: remove dup

--- apps/artemis-web/artemis-web/libs/csrf.ts
+++
@@ -1,9 +0,0 @@
-// file: lib/csrf.js
-import { nextCsrf } from './next-csrf';
-
-const options = {
-  secret: process.env.CSRF_SECRET, // Long, randomly-generated, unique, and unpredictable value
-  csrfSecret: process.env.CSRF_SECRET,
-};
-
-export const { csrf, setup, csrfToken } = nextCsrf(options);
--- apps/artemis-web/artemis-web/pages/api/setup.ts
+++
@@ -1,8 +0,0 @@
-import { setup } from '../../libs/csrf';
-
-const handler = (req, res) => {
-  res.statusCode = 200;
-  res.json({ message: 'CSRF token added to cookies' });
-};
-
-export default setup(handler);
